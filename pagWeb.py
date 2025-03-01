9cf65e167e781fc9798996cb0f1a35423e5cc817
Samantha Neyra Marquez <samantha.kateryn@gmail.com>
2025-02-28 19:53:10 -0500

    segundo cambio hecho desde mi repositorio local

--- pagWeb.py
+++ pagWeb.py
@@ -5,5 +5,9 @@ print("¡Hola, mundo!")
 num1 = 5
 num2 = 3
 suma = num1 + num2
+resta=num1 - num2
 
 print(f"La suma de {num1} y {num2} es {suma}")
+
+##este es un cambio que modifique desde mi repositorio local
+print(f"La resta de {num1} y {num2} es {resta}")
